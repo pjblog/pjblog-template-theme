02c9e2e7549d764c2528e1272df5b2f2d43e88e7
沈赟杰 <shenyunjie@znrank.com>
2023-04-07 16:14:57 +0800

    update

--- vite.config.client.ts
+++ vite.config.client.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-const pkg = require('./package.json');
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => {
